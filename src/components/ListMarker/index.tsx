beec1536115296e16aefc44ef7bbf6f9ac63ad15
nicole595 <nicoletp16@gmail.com>
2023-03-06 12:14:27 -0300

    oi

---
+++ src/components/ListMarker/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Text, View } from "react-native/types";
+import { Marker } from "../Marker";
+import { styles } from "./styles";
+
+export interface ITextMarker {
+    textMarker: string
+}
+
+export function ListMarker({ textMarker }: ITextMarker){
+    return(
+        <View style={styles.listMarker}>
+            <Marker/>
+            <Text style={styles.textMarker}>{textMarker}</Text> 
+        </View>
+    )
+}
---
+++ src/styles/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+    primary: '#2D815D',
+    secondary: '#4B0082',
+    third: '#D9D9D9',
+    thirdLight: 'rgba(217, 217, 217, 0.8)'
+    white: '#ffffff',
+    black: "#000000",
+}
