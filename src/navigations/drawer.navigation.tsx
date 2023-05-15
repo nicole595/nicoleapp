4a0bb0f1820b40ef6f18606d993a4f3392b8500c
nicole595 <nicoletp16@gmail.com>
2023-05-15 12:22:04 -0300

    yes

--- src/navigations/drawer.navigation.tsx
+++ src/navigations/drawer.navigation.tsx
@@ -1,20 +1,43 @@
 import React from 'react';
 import { DrawerNavigationProp, createDrawerNavigator } from '@react-navigation/drawer';
-import { ScreenPerfil } from "../screens"
+import { ScreenPerfil, ScreenCamera } from "../screens"
 import { colors } from '../styles/colors';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, AntDesign } from '@expo/vector-icons';
 type DrawerParamList ={
   Perfil: undefined
-}
+  Camera: undefined
+};
+
 type DrawerScreenNavigationProp = DrawerNavigationProp<DrawerParamList, 'Perfil'>
 export type DrawerTypes = {
   navigation: DrawerScreenNavigationProp
 }
 export function DrawerNavigation() {
-  const Drawer = createDrawerNavigator();
+  const Drawer = createDrawerNavigator<DrawerParamList>();;
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Perfil" component={ScreenPerfil} />
+    <Drawer.Navigator
+      screenOptions={{
+        drawerStyle:{
+          backgroundColor: colors.primary
+        },
+        drawerActiveTintColor: colors.white
+      }}
+      
+    >
+      <Drawer.Screen name="Perfil" component={ScreenPerfil}
+        options={{
+          drawerIcon: () => (
+            <Ionicons name='person' size={24} color={colors.white}/>
+          )
+        }} 
+      />
+      <Drawer.Screen name="Camera" component={ScreenCamera}
+        options={{
+          drawerIcon: () => (
+            <AntDesign name='camera' size={24} color={colors.white}/>
+          )
+        }} 
+      />
     </Drawer.Navigator>
   );
 }
