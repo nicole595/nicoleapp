7371c01afcb948d50455f14f77b6d05de267e90f
nicole595 <nicoletp16@gmail.com>
2023-05-22 12:40:32 -0300

    pronto

--- src/screens/Camera/index.tsx
+++ src/screens/Camera/index.tsx
@@ -2,6 +2,7 @@ import { Camera, CameraCapturedPicture, CameraType } from 'expo-camera';
 import { useRef, useState } from 'react';
 import { Button, Text, TouchableOpacity, View, Image, Alert } from 'react-native';
 import { ComponentButtonInterface, ComponentButtonTakePicture } from '../../components';
+import { AntDesign } from '@expo/vector-icons'; 
 import { ButtonTakePicture } from '../../components/ButtonTakePicture';
 import { styles } from "./styles";
 import * as MediaLibrary from 'expo-media-library';
@@ -13,8 +14,7 @@ export function CameraScreen() {
     const [permissionMedia, requestPermissionMedia] = MediaLibrary.usePermissions();
     const [photo, setPhoto] = useState<CameraCapturedPicture | ImagePicker.ImagePickerAsset>();
     const ref = useRef<Camera>(null);
-    const [takePhoto, setTakePhoto] = useState(false);
-    const []
+    const [verPhoto, setVerPhoto] = useState(1);
 
     if (!permissionCamera) {
         // Camera permissions are still loading
@@ -44,19 +44,21 @@ export function CameraScreen() {
     if (!permissionMedia.granted) {
         // Camera permissions are not granted yet
         return (
-          <View style={styles.container}>
-            <Text style={{ textAlign: 'center' }}>Clique para permitir salvar a imagem</Text>
-            <Button onPress={requestPermissionMedia} title="grant permission" />
-          </View>
+            <View style={styles.container}>
+                <Text style={{ textAlign: 'center' }}>Clique para permitir salvar a imagem</Text>
+                <Button onPress={requestPermissionMedia} title="grant permission" />
+            </View>
         );
-      }
+    }
 
-    
+
 
     async function takePicture() {
-        if(ref.current){
+        if (ref.current) {
             const picture = await ref.current.takePictureAsync()
             setPhoto(picture)
+            setVerPhoto(2)
+
         }
     }
 
@@ -64,7 +66,7 @@ export function CameraScreen() {
         const asset = await MediaLibrary.createAssetAsync(photo!.uri)
         MediaLibrary.createAlbumAsync("Images", asset, false)
         Alert.alert("Imagem salva com sucesso!")
-    }    
+    }
 
     async function pickImage() {
         const result = await ImagePicker.launchImageLibraryAsync({
@@ -72,26 +74,33 @@ export function CameraScreen() {
             aspect: [4, 3],
             quality: 1
         })
-        if (!result.canceled){
+        if (!result.canceled) {
             setPhoto(result.assets[0])
         }
     }
 
     return (
         <View style={styles.container}>
-            if ()
-            
-            <Camera style={styles.camera} type={type} ref={ref}>
-                <ComponentButtonTakePicture onPress={takePicture}/>
-            </Camera>
-            <ComponentButtonInterface title='Abrir imagem' type='secondary' onPressI={pickImage} />
 
-            {photo && photo.uri && (
-                <Image source={{uri: photo.uri}} style={styles.img}/>
+            {verPhoto == 1 ? (
+                <>
+                    <ComponentButtonInterface title='Girar' type='secondary' onPressI={toggleCameraType} />
+                    <Camera style={styles.camera} type={type} ref={ref} >
+                        <ComponentButtonTakePicture onPress={takePicture} />              
+                    </Camera>
+                    <ComponentButtonInterface title='Abrir imagem' type='secondary' onPressI={pickImage} />
+                </>
+            ) : (
+                <>
+                    <TouchableOpacity onPress={() => setVerPhoto(1)}>
+                        <AntDesign name="arrowleft" size={30} color="black" />
+                    </TouchableOpacity>
+                    {photo && photo.uri && (
+                        <Image source={{ uri: photo.uri }} style={styles.img} />
+                    )}
+                    <ComponentButtonInterface title='Salvar imagem' type='secondary' onPressI={savePhoto} />
+                </>
             )}
-            <ComponentButtonInterface title='Girar' type='secondary' onPressI={toggleCameraType}/>
-            <ComponentButtonInterface title='Salvar imagem' type='secondary' onPressI={savePhoto} />
-            
         </View>
-    );
+    )
 }
