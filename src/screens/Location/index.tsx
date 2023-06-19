df54a03dbaf5709c6fc77d1312ef13ae1b58c39b
nicole595 <nicoletp16@gmail.com>
2023-06-19 12:21:03 -0300

    citron

--- src/screens/Location/index.tsx
+++ src/screens/Location/index.tsx
@@ -1,16 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Text, View } from 'react-native';
 import * as Location from 'expo-location';
 import { styles } from "./styles"
-import MapView,{ Region, Marker } from 'react-native-maps';
+import MapView,{ Region, Marker, Polyline } from 'react-native-maps';
+import { colors } from '../../styles/colors';
+import { API_GOOGLE } from '@env'
+import {GooglePlacesAutocomplete, GooglePlaceData, GooglePlaceDetail} from 'react-native-google-places-autocomplete'
+import MapViewDirections from 'react-native-maps-directions'
 
 export function LocationScreen() {
   const [location, setLocation] = useState<Location.LocationObject | null>(null);
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
   const [region, setRegion] = useState<Region>()
   const [marker, setMarker] = useState<Region[]>()
+  const [coords, setCoords] = useState<ICoords[]>([])
+  const [destination, setDestination] = useState<Region | null>(null)
+  const mapRef = useRef<MapView>(null)
+
+  type ICoords = {
+    latitude: number
+    longitude: number
+  }
 
   useEffect(() => {
+    let subscription: Location.LocationSubscription
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
@@ -32,7 +45,20 @@ export function LocationScreen() {
         latitudeDelta: 0.004,
         longitudeDelta: 0.004
         }])
+      subscription = await Location.watchPositionAsync({
+        accuracy: Location.LocationAccuracy.High,
+        timeInterval: 1000,
+        distanceInterval: 1
+      }, (location) =>{
+        setCoords((prevState) => [...prevState, location.coords])
+      });
     })();
+
+    return () => {
+      if (subscription) {
+        subscription.remove()
+      }
+    }
   }, []);
   let text = 'Waiting..';
   if (errorMsg) {
@@ -44,10 +70,15 @@ export function LocationScreen() {
   return (
     <View style={styles.container}>
         {region ? (
-            <MapView region={region} style={styles.map} >
+            <MapView region={region} style={styles.map} showsUserLocation={true}>
                 {marker && marker.map((i) => (
                     <Marker key={i.latitude} coordinate={i} />
                 ))}
+                {coords && <Polyline
+                  coordinates={coords}
+                  strokeColor={colors.primary}
+                  strokeWidth={7}
+                />}
             </MapView>
         ) : (
             <Text style={styles.paragraph}>{text}</Text>
